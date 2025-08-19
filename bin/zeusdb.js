bd0e0a3dfa34c8f700355e5fd8f01e8404045261
HenryPrice-MM <henry.price@mastermind.com>
2025-08-19 18:27:40 -0400

    fixed export module type

--- bin/zeusdb.js
+++ bin/zeusdb.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 
 import { spawn } from 'child_process';
-import { createRequire } from 'module';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-const require = createRequire(import.meta.url);
-
-process.removeAllListeners('warning');
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const entryPath = resolve(__dirname, `../src/cli/index.ts`);
-const tsxBin = require.resolve('tsx/cli');
+const entryPath = resolve(__dirname, `../dist/cli/index.js`);
 
-spawn('node', [tsxBin, entryPath, ...process.argv.slice(2)], {
+spawn('node', [entryPath, ...process.argv.slice(2)], {
   stdio: 'inherit'
 });
